272f133c071980a8b1ae1b1d17ec532a3fc7486c
Sejal Gupta <sejalguptaofficial11@gmail.com>
2024-12-22 15:48:41 +0530

    creating routes and middlewares

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import "dotenv/config";
 
 import connectDB from "./config/mongodb.js";
+import userRouter from "./routes/userRoutes.js";
+import imageRouter from "./routes/imageRoutes.js";
 
 const PORT = process.env.PORT || 4000;
 const app = express();
@@ -11,6 +13,9 @@ app.use(express.json());
 app.use(cors());
 
 await connectDB();
+
+app.use('/api/user', userRouter);
+app.use('/api/image', imageRouter);
 
 app.get("/", (req, res) => {
   res.send("API working");
